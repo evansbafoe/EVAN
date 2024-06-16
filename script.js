011a1af2119867ee800a53d15e9ae31b6be32d96
evansbafoe <baffourevans06@gmail.com>
2024-06-16 10:57:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -31,10 +31,6 @@ const cart = [];
 function showPage(pageId) {
   document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
   document.getElementById(pageId).classList.add('active');
-  document.addEventListener("keydown",function(event)
-    {
-      if (event.key === "ArrowLeft" event.key === "Backspace") 
-}
   
 }
 
