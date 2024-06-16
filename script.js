67c50fc8e98223da3c6f36f9ff5de27e35890fa9
evansbafoe <baffourevans06@gmail.com>
2024-06-16 08:59:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const cart = [];
 function showPage(pageId) {
   document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
   document.getElementById(pageId).classList.add('active');
+  document.body.style.transform ="scaleX(-1)";
 }
 
 function addToCart(productName, productPrice) {
